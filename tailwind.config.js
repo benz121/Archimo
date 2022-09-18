80f3b117f77c4c3e81a8ea22eba5b400afa57b38
b3nz <benzuday@gmail.com>
2022-09-18 18:28:06 +0530

    First Cry

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.html',
+    './*.html'
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
